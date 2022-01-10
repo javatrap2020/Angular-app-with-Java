07d343219c0b3bc164d30a33bb4c4f5a8ed76718
Oleh Boiko <javatrap2020@gmail.com>
2022-01-10 19:09:16 +0200

    add repository UsersRepository

---
+++ src/main/java/com/javatrap2020/angular_app_with_java/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.javatrap2020.angular_app_with_java.repository;
+
+import com.javatrap2020.angular_app_with_java.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UsersRepository extends JpaRepository<Users, Integer>, JpaSpecificationExecutor<Users>, QuerydslPredicateExecutor<Users> {
+}
