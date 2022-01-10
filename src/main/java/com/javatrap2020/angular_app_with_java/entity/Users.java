6cb9e52294094edece7f5fee5786e69af5694954
Oleh Boiko <javatrap2020@gmail.com>
2022-01-10 18:57:33 +0200

    add entity Users

---
+++ src/main/java/com/javatrap2020/angular_app_with_java/entity/Users.java
@@ -0,0 +1,26 @@
+package com.javatrap2020.angular_app_with_java.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class Users {
+    @Id
+    @Column
+    private long id;
+    @Column
+    @NotNull(message = "{NotNull.User.firstName}")
+    private String firstName;
+    @Column
+    @NotNull(message = "{NotNull.User.lastName}")
+    private String lastName;
+    @Column
+    @NotNull(message = "{NotNull.User.email}")
+    private String email;
+
+}
