fcefc2845a2d1582a93294e60e9748adf9384f1b
Oleh Boiko <javatrap2020@gmail.com>
2022-01-11 18:58:20 +0200

    fixing pom, UsersRepository, data.sql and add AppController

---
+++ src/main/java/com/javatrap2020/angular_app_with_java/AppController.java
@@ -0,0 +1,17 @@
+package com.javatrap2020.angular_app_with_java;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class AppController {
+    @RequestMapping({"/"})
+    public String loadUI() {
+        log.info("loading UIssdd");
+        return "forward:/index.html";
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS  users;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY ,
-    FIRST_NAME VARCHAR (250) NOT NULL,
-    LAST_NAME VARCHAR (250) NOT NULL,
-    EMAIL VARCHAR (250) NOT NULL,
+                       id INT PRIMARY KEY ,
+                       first_name VARCHAR (250) NOT NULL,
+                       last_name VARCHAR (250) NOT NULL,
+                       email VARCHAR (250) NOT NULL
 );
 
-INSERT INTO users (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES
-    (1, 'Java1', 'Trap1', 'java1@gmail.com'),
-    (2, 'Java2', 'Trap2', 'java2@gmail.com'),
-    (3, 'Java3', 'Trap3', 'java3@gmail.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES
+(1, 'Java1', 'Trap1', 'java1@gmail.com'),
+(2, 'Java2', 'Trap2', 'java2@gmail.com'),
+(3, 'Java3', 'Trap3', 'java3@gmail.com');
